841e965586366220a986d4aaca828fd673b0f00f
msfArmando <armandomonsaof@gmail.com>
2026-09-24 17:23:23 -0300

    feat: melhorando consulta com novas regras

--- sql-alchemy-pratices/sql_alchemy_pratices/scripts.py
+++ sql-alchemy-pratices/sql_alchemy_pratices/scripts.py
@@ -1,38 +1,56 @@
-from sqlalchemy import create_engine, MetaData, Table, select, Column, Float
 import json
+
+from sqlalchemy import (
+    Column,
+    Float,
+    MetaData,
+    Table,
+    and_,
+    create_engine,
+    desc,
+    select,
+    case,
+    alias
+)
 
 DATABASE_URL = 'sqlite+pysqlite:///brazil_oil_production.sqlite'
 
 engine = create_engine(DATABASE_URL, echo=True)
 metadata_obj = MetaData()
 
-production = Table("production", metadata_obj, Column('Oil (m³)', Float),
-                    Column('Condensate oil (m³)', Float), 
-                    autoload_with=engine)
+production = Table(
+    'production',
+    metadata_obj,
+    Column('Oil (m³)', Float),
+    Column('Condensate oil (m³)', Float),
+    autoload_with=engine,
+)
 
-stmt = select(production.columns["Field"],
-              production.columns["Basin"],
-              production.columns["Installation"],
-              production.columns["Oil (m³)"],
-              production.columns["Condensate oil (m³)"],
-              production.columns["Associated petroleum gas (Mm³)"],
-              production.columns["Non-associated petroleum gas (Mm³)"],
-              production.columns["Water (m³)"],
-              production.columns["Gas injection (Mm³)"],
-              production.columns["Secondary recovery water injection (m³)"],
-              production.columns["Wastewater injection (m³)"],
-              production.columns["CO2 injection (Mm³)"],
-              production.columns["Nitrogen injection (Mm³)"],
-              production.columns["Steam injection (t)"],
-              production.columns["Polymer injection (m³)"],
-              production.columns["Others fluids injection (m³)"]).where(production.c.State == "PB")
+stmt = (
+    select(
+        case(
+            (production.columns['Basin'] == 'Santos', 'Charlie Brown Jr.'),
+            else_=production.columns['Basin']
+        ).label('Município'),
+        production.columns['State'].label('Estado'),
+        case(
+            (production.columns['Field'] == 'LULA', 'Ladrão'), else_=production.columns['Field']
+        ).label('Campo'),
+        production.columns['Installation'].label('Instalação'),
+        production.columns['Oil (m³)'],
+    )
+    .where(
+        and_(
+        production.c.State.in_(['SP', 'RJ']), production.columns['Oil (m³)'].is_not(None))
+    )
+    .order_by(desc(production.columns['Oil (m³)']))
+).limit(10000)
 
 with engine.connect() as conn:
-  
     dictlist = []
     for row in conn.execute(stmt).fetchall():
         dictitem = dict(row._mapping.items())
         dictlist.append(dictitem)
 
-    with open("result.json", "w", encoding="utf-8") as f:
+    with open('result.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(dictlist, ensure_ascii=False))
